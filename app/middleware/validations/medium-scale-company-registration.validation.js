3e306911aaadee17da3ede769c8d6f4e24499f82
Federex A Potolin <36563535+sirexlangnmn@users.noreply.github.com>
2023-02-03 14:04:24 +0800

    remove normalizeemail

--- app/middleware/validations/medium-scale-company-registration.validation.js
+++ app/middleware/validations/medium-scale-company-registration.validation.js
@@ -3,7 +3,7 @@ const { check, validationResult } = require('express-validator');
 const validationMiddleware = [
     check('companyName').not().isEmpty().withMessage('S: Company Name is required.').trim().escape(),
     check('businessWebsite').not().isEmpty().withMessage('S: Business Website is required.').trim().escape(),
-    check('businessEmailAddress').not().isEmpty().withMessage('S: Business Email Address is required').trim().escape().isEmail().normalizeEmail().withMessage('S: Invalid Email Address.'),
+    check('businessEmailAddress').not().isEmpty().withMessage('S: Business Email Address is required').trim().escape().isEmail().withMessage('S: Invalid Email Address.'),
     check('businessContactNumber').not().isEmpty().withMessage('S: First Name is required.').trim().escape(),
     check('businessSocialMediaContactNumber').not().isEmpty().withMessage('S: Social Media Contact number is required.').trim().escape(),
     check('businessCountryLocation').not().isEmpty().withMessage('S: Business Country Location is required.').trim().escape(),
@@ -17,7 +17,7 @@ const validationMiddleware = [
     check('states').not().isEmpty().withMessage('S: States is required.').trim().escape(),
     check('city').not().isEmpty().withMessage('S: City is required.').trim().escape(),
     check('language').not().isEmpty().withMessage('S: Language is required.'),    
-    check('emailAddress').not().isEmpty().withMessage('S: Email Address is required').trim().escape().isEmail().normalizeEmail().withMessage('S: Invalid Email Address.'),
+    check('emailAddress').not().isEmpty().withMessage('S: Email Address is required').trim().escape().isEmail().withMessage('S: Invalid Email Address.'),
     check('personalSocialMediaContactNumber').not().isEmpty().withMessage('S: Personal Contact number is required.').trim().escape(),    
     check('password').not().isEmpty().withMessage('S: Password is required.'),
     check('confirmPassword', 'S: Passwords do not match').custom((value, { req }) => value === req.body.password),
--- app/middleware/validations/small-scale-company-registration.validation.js
+++ app/middleware/validations/small-scale-company-registration.validation.js
@@ -15,7 +15,7 @@ const validationMiddleware = [
     check('states').not().isEmpty().withMessage('S: States is required.').trim().escape(),
     check('city').not().isEmpty().withMessage('S: City is required.').trim().escape(),
     check('language').not().isEmpty().withMessage('S: Language is required.'),    
-    check('emailAddress').not().isEmpty().withMessage('S: Email Address is required').trim().escape().isEmail().normalizeEmail().withMessage('S: Invalid Email Address.'),
+    check('emailAddress').not().isEmpty().withMessage('S: Email Address is required').trim().escape().isEmail().withMessage('S: Invalid Email Address.'),
     check('socialMediaContactNumber').not().isEmpty().withMessage('S: Contact number is required.').trim().escape(),
     check('password').not().isEmpty().withMessage('S: Password is required.'),
     check('confirmPassword', 'S: Passwords do not match').custom((value, { req }) => value === req.body.password),
